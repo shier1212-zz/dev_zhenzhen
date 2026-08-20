7f30d8d6e0d1bde3bbc3f5f4a4cd47b7c5db96b8
lzlzz <2660815665@qq.com>
2026-08-20 11:28:51 +0800

    feat(admin): App.jsx 注册 6 个模块的新增/编辑路由
    - 轮播/新闻/分类/部门/账号/角色 各加 /xxx/new 与 /xxx/:id/edit 路由
    
    - 配合 MainLayout 路径前缀匹配实现子页面菜单高亮

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,18 +5,24 @@ import MainLayout from "./components/MainLayout";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import BannerManage from "./pages/BannerManage";
+import BannerForm from "./pages/BannerForm";
 import NewsManage from "./pages/NewsManage";
+import NewsForm from "./pages/NewsForm";
 import HomeBrand from "./pages/HomeBrand";
 import HomeFeatured from "./pages/HomeFeatured";
 import AboutEdit from "./pages/AboutEdit";
 import ContactConfig from "./pages/ContactConfig";
 import CategoryManage from "./pages/CategoryManage";
+import CategoryForm from "./pages/CategoryForm";
 import ProductManage from "./pages/ProductManage";
 import ProductForm from "./pages/ProductForm";
 import MessageManage from "./pages/MessageManage";
 import DepartmentManage from "./pages/DepartmentManage";
+import DepartmentForm from "./pages/DepartmentForm";
 import UserManage from "./pages/UserManage";
+import UserForm from "./pages/UserForm";
 import RoleManage from "./pages/RoleManage";
+import RoleForm from "./pages/RoleForm";
 import LogManage from "./pages/LogManage";
 
 export default function App() {
@@ -32,19 +38,31 @@ export default function App() {
       >
         <Route path="/" element={<Dashboard />} />
         <Route path="/banner" element={<BannerManage />} />
+        <Route path="/banner/new" element={<BannerForm />} />
+        <Route path="/banner/:id/edit" element={<BannerForm />} />
         <Route path="/news" element={<NewsManage />} />
+        <Route path="/news/new" element={<NewsForm />} />
+        <Route path="/news/:id/edit" element={<NewsForm />} />
         <Route path="/home/brand" element={<HomeBrand />} />
         <Route path="/home/featured" element={<HomeFeatured />} />
         <Route path="/about" element={<AboutEdit />} />
         <Route path="/contact" element={<ContactConfig />} />
         <Route path="/category" element={<CategoryManage />} />
+        <Route path="/category/new" element={<CategoryForm />} />
+        <Route path="/category/:id/edit" element={<CategoryForm />} />
         <Route path="/product" element={<ProductManage />} />
         <Route path="/product/new" element={<ProductForm />} />
         <Route path="/product/:id/edit" element={<ProductForm />} />
         <Route path="/message" element={<MessageManage />} />
         <Route path="/department" element={<DepartmentManage />} />
+        <Route path="/department/new" element={<DepartmentForm />} />
+        <Route path="/department/:id/edit" element={<DepartmentForm />} />
         <Route path="/user" element={<UserManage />} />
+        <Route path="/user/new" element={<UserForm />} />
+        <Route path="/user/:id/edit" element={<UserForm />} />
         <Route path="/role" element={<RoleManage />} />
+        <Route path="/role/new" element={<RoleForm />} />
+        <Route path="/role/:id/edit" element={<RoleForm />} />
         <Route path="/log" element={<LogManage />} />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
